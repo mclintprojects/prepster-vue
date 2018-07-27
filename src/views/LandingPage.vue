eaeeb81443996dfb6f0565aacf577fb9e7bbc37a
Mbah Clinton <clintonmbah44@gmail.com>
2018-07-27 07:13:36 +0000

    added sbg background to landing page

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -46,7 +46,9 @@ export default {
 	flex-direction: column;
 	min-height: calc(100vh - 55px);
 	text-align: center;
-	background: var(--primary-color);
+	background-color: #b8eeee;
+	background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='390' height='46.8' viewBox='0 0 1000 120'%3E%3Cg fill='none' stroke='%23ffffff' stroke-width='10' %3E%3Cpath d='M-500 75c0 0 125-30 250-30S0 75 0 75s125 30 250 30s250-30 250-30s125-30 250-30s250 30 250 30s125 30 250 30s250-30 250-30'/%3E%3Cpath d='M-500 45c0 0 125-30 250-30S0 45 0 45s125 30 250 30s250-30 250-30s125-30 250-30s250 30 250 30s125 30 250 30s250-30 250-30'/%3E%3Cpath d='M-500 105c0 0 125-30 250-30S0 105 0 105s125 30 250 30s250-30 250-30s125-30 250-30s250 30 250 30s125 30 250 30s250-30 250-30'/%3E%3Cpath d='M-500 15c0 0 125-30 250-30S0 15 0 15s125 30 250 30s250-30 250-30s125-30 250-30s250 30 250 30s125 30 250 30s250-30 250-30'/%3E%3Cpath d='M-500-15c0 0 125-30 250-30S0-15 0-15s125 30 250 30s250-30 250-30s125-30 250-30s250 30 250 30s125 30 250 30s250-30 250-30'/%3E%3Cpath d='M-500 135c0 0 125-30 250-30S0 135 0 135s125 30 250 30s250-30 250-30s125-30 250-30s250 30 250 30s125 30 250 30s250-30 250-30'/%3E%3C/g%3E%3C/svg%3E");
+	background-size: auto auto;
 
 	img {
 		+ p {
@@ -62,7 +64,7 @@ export default {
 			+ p {
 				color: var(--secondary-text-color);
 				font-size: 16px;
-				width: 30%;
+				width: 40%;
 			}
 		}
 	}
